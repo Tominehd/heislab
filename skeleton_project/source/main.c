5f42d9f23b2de48f319c29a0d95cc0315888e8d7
mariausy <79471562+mariausy@users.noreply.github.com>
2022-03-10 11:37:40 +0100

    Add files via upload

--- skeleton_project/source/main.c
+++ skeleton_project/source/main.c
@@ -13,12 +13,19 @@
 // Klare å ta imot ordere mellom etasjer
 // Få en ordning som sorterer koden
 
-int orders[4][4] = {
-    {0,0,0,0},
-    {1,0,0,0},
-    {2,0,0,0},
-    {3,0,0,0}
-};
+//Køen. ordersMatrix[i][0] = etasje, ordersMatrix[i][1] = type knapp trykket
+int ordersMatrix[12][2] = {{9,9},
+                           {9,9},
+                           {9,9},
+                           {9,9},
+                           {9,9},
+                           {9,9},
+                           {9,9},
+                           {9,9},
+                           {9,9},
+                           {9,9},
+                           {9,9},
+                           {9,9}};
 
 
 int lastFloor = 0;
@@ -27,15 +34,15 @@ int getCurrentFloor(){
     return elevio_floorSensor();
 }
 
+void closeElevatorDoor(){
+    elevio_doorOpenLamp(0);
+};
 
 
 void openElevatorDoor(){
     elevio_doorOpenLamp(1);
 };
 
-void closeElevatorDoor(){
-    elevio_doorOpenLamp(0);
-};
 
 
 int isAtTargetFloor(int targetFloor){
@@ -45,17 +52,6 @@ int isAtTargetFloor(int targetFloor){
     else{
         return 0;
     }
-}
-
-void hasReachedTargetFloor(){
-        openElevatorDoor();
-        elevio_motorDirection(DIRN_STOP);
-        printf("Er her");
-
-        struct timespec remaining, request = {3, 500};
-        int response = nanosleep(&request, &remaining);
-
-        closeElevatorDoor();
 }
 
 void valid_floor(){
@@ -76,7 +72,6 @@ int get_lastFloor(){
     int currentFloor = getCurrentFloor();
     if(currentFloor != -1){
         lastFloor  = currentFloor;
-        printf("Etter last floor %d \n", lastFloor);
         return lastFloor;
     }
     return lastFloor;
@@ -91,6 +86,18 @@ void elevatorLight(){
     }
 }
 
+void hasReachedTargetFloor(){
+    openElevatorDoor();
+    elevio_motorDirection(DIRN_STOP);
+    printf("Er her");
+    elevatorLight();
+    struct timespec remaining, request = {3, 500};
+    nanosleep(&request, &remaining);
+
+    closeElevatorDoor();
+}
+
+
 void stopElevator(){
     elevio_stopLamp(1);
     if(getCurrentFloor() != -1){
@@ -98,51 +105,132 @@ void stopElevator(){
     }
 }
 
-void driveElevator(int targetFloor, ButtonType b, int lastFloor){
-
-    printf("Blir funksjonen kjørt");
-
-    //if(b == BUTTON_CAB){
-        printf("Knappen er cab");
-
-
-        while(!(isAtTargetFloor(targetFloor))){
-
-        // Først vil vi sjekke om vi skal opp eller ned
-        
-        //Floor -1 betyr at den er i en mellom stadie
-            printf("Targetfloor: %d \n",targetFloor);
-            printf("Floor: %d \n",getCurrentFloor());
-            printf("Last floor: %d \n",get_lastFloor());
-
-
-            if (get_lastFloor() > targetFloor){
-                elevio_motorDirection(DIRN_DOWN);
+int addToOrders(){
+    for(int f = 0; f < N_FLOORS; f++){
+        for(int b = 0; b < N_BUTTONS; b++){
+            int btnPressed = elevio_callButton(f, b);
+            elevio_buttonLamp(f, b, btnPressed);
+            if (btnPressed){
+                //printf("registert at knapp er trykket på");
+                //Sjekker før om denne ordren allerede er lagt til
+                for (int i = 0; i < 12; i++){
+                    for (int j = 0; j < 2; j++){
+                        if (ordersMatrix[i][0] == f && ordersMatrix[i][1] == b){
+                            return 0; //returnerer 0 om orderen er lagt til og går ut av funksjonen
+                        }
+                    }
+                }
+                //Om orederen ikke finnes allrede legges den til den første linja i matrisa som ikke inneholder en ordre ({6,6})
+                for (int i = 0; i < 12; i++){
+                    for (int j = 0; j < 2; j++){
+                        if (ordersMatrix[i][0] == 9 && ordersMatrix[i][1] == 9){
+                            ordersMatrix[i][0] = f;
+                            ordersMatrix[i][1] = b;
+                            return 1;
+                        }
+                    }
+                }
             }
+        }
+    }
+    return 1; //Bare for at noe skal returneres uansett
+}
 
-            else if( get_lastFloor() < targetFloor){
-                elevio_motorDirection(DIRN_UP);
+
+//Returnerer 0 for oppover, 1 for nedover (for å matche ButtonType), 2 for i ro
+int getElevDirection(){
+    int thisFloor = get_lastFloor();
+    if (ordersMatrix[0][0] > thisFloor){
+        return 0;
+    }
+    else if (ordersMatrix[0][0] < thisFloor){
+        return 1;
+    }
+    return 3;
+}
+
+
+int checkOrdersThisFloor(){
+    int thisFloor = get_lastFloor();
+    int ElevDirection = getElevDirection();
+    int anyOrders = 0; //Forblir 0 om det ikke er noen ordre i denne etasjen, blir 1 om det er det
+
+    for (int i = 0; i < 12; ++i){
+        //tror denne if setningen virker
+        if(ordersMatrix[i][0] == thisFloor && (ordersMatrix[i][1] == ElevDirection || ElevDirection == 3 || ordersMatrix[i][1] == 2)){
+            //itererer gjennom ordrene i køen herfra og ned og flytter de et steg opp for å "rydde opp"
+            for (int j = (i+1); j < 12; j++){
+                ordersMatrix[j-1][0] = ordersMatrix[j][0];
+                ordersMatrix[j-1][1] = ordersMatrix[j][1];
             }
+            ordersMatrix[11][0] = 9;
+            ordersMatrix[11][1] = 9;
+            anyOrders = 1;
+        }
+    }
 
-            nanosleep(&(struct timespec){0, 20*1000*1000}, NULL);
+    for (int i = 0; i < 12; i++){
+        for (int j = 0; j < 2; j++){
+            int a = ordersMatrix[i][j];
+            printf(" %d ", a);
+            printf("%d", i);
+            printf("%d", j);
+        }
+    printf("\n");
+    }
+
+    return anyOrders;
+}
+
+void driveElevator(int targetFloor, ButtonType b){
+
+    while(!(isAtTargetFloor(targetFloor))){
+
+    // Først vil vi sjekke om vi skal opp eller ned
     
-            elevatorLight();
-        }  
-        
-        printf("!!!!!!!!!!!!!!!!!!!!!");
-         if ( getCurrentFloor() == targetFloor){
-                elevio_motorDirection(DIRN_STOP);
-                printf("er her");
-                hasReachedTargetFloor();
+    //Floor -1 betyr at den er i en mellom stadie
+        printf("Targetfloor: %d \n",targetFloor);
+        printf("Floor: %d \n",getCurrentFloor());
+        printf("Last floor: %d \n",get_lastFloor());
 
-                elevatorLight();
+
+        if (get_lastFloor() > targetFloor){
+            elevio_motorDirection(DIRN_DOWN);
+        }
+
+        else if( get_lastFloor() < targetFloor){
+            elevio_motorDirection(DIRN_UP);
+        }
+
+
+        if(getCurrentFloor() != -1){
+            int ordersHere = checkOrdersThisFloor();
+            checkOrdersThisFloor();
+            if(ordersHere){
+                hasReachedTargetFloor();
             }
 
-        
-     
+        }
+        nanosleep(&(struct timespec){0, 20*1000*1000}, NULL);
 
-    //}
+        elevatorLight();
 
+        addToOrders();
+    }  
+    
+    printf("!!!!!!!!!!!!!!!!!!!!!");
+    if (getCurrentFloor() == targetFloor){
+        elevio_motorDirection(DIRN_STOP);
+        printf("er her");
+        checkOrdersThisFloor();
+        hasReachedTargetFloor();
+      
+    }
+
+    
+    
+
+//}
 }
 
 
@@ -161,11 +249,12 @@ int main(){
     valid_floor();
 
     while(1){
+
         int floor = elevio_floorSensor();
         
 
-        if(floor != -1){
-            printf("floor: %d \n",floor); }
+//        if(floor != -1){
+//            printf("floor: %d \n",floor); }
 
         if(floor == 0){
             elevio_motorDirection(DIRN_STOP);
@@ -175,7 +264,14 @@ int main(){
             elevio_motorDirection(DIRN_STOP);
         }
 
+       
+        addToOrders();
+        if(ordersMatrix[0][0] != 9){
+            closeElevatorDoor();
+            driveElevator(ordersMatrix[0][0], ordersMatrix[0][1]);
+        }
 
+        /**
         for(int f = 0; f < N_FLOORS; f++){
             for(int b = 0; b < N_BUTTONS; b++){
                 int btnPressed = elevio_callButton(f, b);
@@ -183,12 +279,14 @@ int main(){
                 if(btnPressed){
                     printf("Knapp er trykket på, ønsker å dra til: %d \n",f);
                     closeElevatorDoor();
-
+                    addToOrders();
                     driveElevator(f,b,lastFloor);
                 }
             }
         }
+
         
+        */
 
         if(elevio_obstruction()){
             elevio_stopLamp(1);
@@ -203,10 +301,20 @@ int main(){
         
         
 
+
         
         nanosleep(&(struct timespec){0, 20*1000*1000}, NULL);
     }
 
+    for (int i = 0; i < 12; i++){
+        for (int j = 0; j < 2; j++){
+            int a = ordersMatrix[i][j];
+            printf(" %d ", a);
+            printf("%d", i);
+            printf("%d", j);
+        }
+    printf("\n");
+    }   
 
     return 0;
 }
