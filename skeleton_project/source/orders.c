71c3aba7bcc6bfa4960df44f1b73363dffec065d
Tominehd <Tominehd@stud.ntnu.no>
2022-03-15 01:11:47 +0100

    mer funker

--- skeleton_project/source/orders.c
+++ skeleton_project/source/orders.c
@@ -1,6 +1,8 @@
 #include "orders.h"
 
 
+
+// Er bare mulig å ha 10 ordere Håper at dette kan korte ned kjøretiden litt
 static int ordersMatrix[10][2] = {{9,9},
                            {9,9},
                            {9,9},
